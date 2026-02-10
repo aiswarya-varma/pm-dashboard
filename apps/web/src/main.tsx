1371adf7c3adfdfa20ff121992aa45c3ae221807
aiswarya-varma <aiswaryavarma44@gmail.com>
2026-02-10 19:02:36 +0530

    [FE] Remove unused import

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import * as ReactDOM from 'react-dom/client';
-import App from './app/app';
 import { Login } from './auth/Login';
 
 const root = ReactDOM.createRoot(
